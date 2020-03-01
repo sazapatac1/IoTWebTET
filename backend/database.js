8494c41285a2469b9618767efeeee0975bd0dbed
Santiago Zapata <saz4560@hotmail.com>
2020-03-01 12:31:02 -0500

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
-//const URI = 'mongodb://database:27017/iotwebtet_DB'
+const URI = 'mongodb://database:27017/iotwebtet_DB'
+//const URI = 'mongodb://localhost:27017/iotwebtet_DB'
 // database in Atlas
-const URI = 'mongodb+srv://sazapatac1:ofM2upbV96tCY9EJ@iotwebtetcluster-c4zmi.mongodb.net/test?retryWrites=true&w=majority'
+//const URI = 'mongodb+srv://sazapatac1:ofM2upbV96tCY9EJ@iotwebtetcluster-c4zmi.mongodb.net/test?retryWrites=true&w=majority'
 
 mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true})
     .then(db => console.log('Database is connected'))
